010dea3f5d640d6d82277eb93689e3fe4d1a7715
João Gil <joaogilmesquita7@gmail.com>
2019-12-15 22:28:52 +0000

    boo movement, added collisions

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ l_arrow = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/l_
 r_arrow = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/r_arrow.png",1,1)
 #l_bow = [pygame.image.load("zelda_Lbowb4.png"),pygame.image.load("zelda_Lbowb3.png"),pygame.image.load("zelda_Lbowb2.png"),pygame.image.load("zelda_Lbowb1.png")]
 r_ghost = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/r_ghostb.gif",1,1)
- 
+l_ghost = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/l_ghostb.png",1,1)
 
 
 class enemy(object):
@@ -63,7 +63,7 @@ class enemy(object):
         self.path = [self.x, self.end]
         self.walkCount = 0
         self.vel = 3
-        self.health = 9
+        self.health = 10
         self.visible = True
         self.hitbox = (self.x+17,self.y +2, self.width,self.height)
     
@@ -71,11 +71,42 @@ class enemy(object):
         self.move()
 
         if self.visible:
-            r_ghost.draw(win,0, self.x, self.y,4)
-    
+            if self.vel > 0:
+                r_ghost.draw(win,0, self.x, self.y,4)
+            else:
+                l_ghost.draw(win,0, self.x, self.y,4)
+
+        self.hitbox = (self.x-self.width/2,self.y-self.height/2,self.width,self.height)
+        #pygame.draw.rect(win,(255,0,0),self.hitbox,2)
 
     def move(self):
-        pass
+        if self.vel > 0:
+            if self.x + self.vel < self.path[1]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.x += self.vel
+        else:
+            if self.x + self.vel < self.path[0]:
+                self.vel = self.vel * -1
+                self.x += self.vel
+            else:
+                self.x += self.vel
+
+    def arrow_hit(self):
+        print("arrow hit")
+        if self.health > 0:
+            self.health -= 5
+        if self.health <= 0:
+            self.visible = False
+    def punch_hit(self):
+        print("FALCON PUUUUNCH")
+        if self.health > 0:
+            self.health -= 2.5
+        if self.health <= 0:
+            self.visible = False
+
+
 
 
 class player(object):
@@ -89,6 +120,7 @@ class player(object):
         self.right = False
         self.standing = True
         self.punch = False  #new
+        self.punchCount = 0
         self.bow = False
         self.bowCount = 0
         self.arrow = False
@@ -97,7 +129,6 @@ class player(object):
         #self.kickCount = 0
         self.isJump = False
         self.jumpCount = 10
-        self.hitbox = (self.x+17,self.y+11,40,40)
     def draw(self,win):
         if self.walkCount +1 >= 30:
             self.walkCount = 0
@@ -151,6 +182,8 @@ class player(object):
                     self.arrow = False
                 r_bow.draw(win,int(-self.bowCount//15),self.x,self.y,4)
                 self.bowCount += 1
+        self.hitbox = (self.x-self.width/2,self.y+10-self.height/2,self.width,self.height)
+        #pygame.draw.rect(win,(255,0,0),self.hitbox,2)
 
 class projectiles(object):
     def __init__(self,x,y,facing):
@@ -158,11 +191,15 @@ class projectiles(object):
         self.y = y
         self.facing = facing
         self.vel = 10 *facing
+        self.width = 80
+        self.height = 30
     def draw(self,win):
         if self.facing == 1:
             r_arrow.draw(win,0,self.x,self.y,4)
         elif self.facing == -1:
             l_arrow.draw(win,0,self.x,self.y,4)
+        self.hitbox = (self.x-self.width/2,self.y+10-self.height/2,self.width,self.height)
+        #pygame.draw.rect(win,(255,0,0),self.hitbox,2)
 
 
 def redrawGameWindow():
@@ -180,9 +217,9 @@ def redrawGameWindow():
     
 
 #main loop
-man = player(300,500,100,150)
+man = player(300,500,110,150)   #Check width and height
 arrows = []
-boo = enemy(100,500,64,64,450)
+boo = enemy(100,485,90,90,450)
 
 run = True
 
@@ -201,10 +238,17 @@ while run:
         else:
             arrows.pop(arrows.index(arrow)) #remove the arrow if it goes outside bounds
     
+    for arrow in arrows:
+        if boo.visible:
+            if arrow.hitbox[0] > boo.hitbox[0] and arrow.hitbox[0] < boo.hitbox[0] + boo.hitbox[2]:
+                boo.arrow_hit()
+                arrows.pop(arrows.index(arrow)) #remove the arrow
 
-
-
-
+    if boo.visible:
+        if man.punch and man.punchCount == 1:
+            if man.hitbox[0] >= boo.hitbox[0] and man.hitbox[0] < boo.hitbox[0] + boo.hitbox[2]:
+                boo.punch_hit()       
+                
 
     keys = pygame.key.get_pressed()
     
@@ -214,6 +258,7 @@ while run:
         man.right = False
         man.standing = False
         man.punch = False
+        man.punchCount = 0
         man.bow = False
     elif keys[pygame.K_RIGHT] and man.x < W - man.width - man.vel: 
         man.x += man.vel
@@ -221,22 +266,27 @@ while run:
         man.right = True
         man.standing = False
         man.punch = False
+        man.punchCount = 0
         man.bow = False
     elif keys[pygame.K_a]:
+        man.punchCount += 1
         man.punch = True
         man.bow = False
         man.standing = False
     elif keys[pygame.K_s]:
         man.bow = True
         man.punch = False
+        man.punchCount = 0
         man.standing = False
-        
     else:
         man.standing = True
         man.bow = False
         man.punch = False
+        man.punchCount = 0
         man.walkCount = 0
         #man.punchCount = 0
         #man.kickCount = 0
+
+
 
     redrawGameWindow()
