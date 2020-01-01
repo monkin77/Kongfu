8ab67333b5b05a0f9b96467ec4cf0e11ec7de002
João Gil <joaogilmesquita7@gmail.com>
2020-01-01 11:50:11 +0000

    Game Over window

--- main.py
+++ main.py
@@ -40,25 +40,25 @@ class spritesheet:
         surface.blit(self.sheet, (x+self.handle[handle][0], y+ self.handle[handle][1]), self.cells[cellIndex])
         
 #walkRight = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/zelda_Rwalk2b.png",6,1)
-walkRight = spritesheet("sprites/zelda_Rwalk2b.png",6,1)
-walkLeft = spritesheet("sprites/zelda_Lwalk2b.png",6,1)     #100 X 120 px
-standing = spritesheet("sprites/zelda_standing2.png",1,1)
-background = pygame.image.load("sprites/wallpaper.gif")
-l_punch = spritesheet("sprites/zelda_Lpunchb.png",1,1)
-r_punch = spritesheet("sprites/zelda_Rpunchb.png",1,1)
-l_bow = spritesheet("sprites/zelda_Lbowb.png",4,1)
-r_bow = spritesheet("sprites/zelda_Rbowb.png",4,1)       #corrigir as imagens
-l_arrow = spritesheet("sprites/l_arrow.png",1,1)
-r_arrow = spritesheet("sprites/r_arrow.png",1,1)
-r_ghost = spritesheet("sprites/r_ghostb.gif",1,1)
-l_ghost = spritesheet("sprites/l_ghostb.png",1,1)
-r_koopa = spritesheet("sprites/koopas_rightb.gif",3,1)
-l_koopa = spritesheet("sprites/koopas_leftb.gif",3,1)
-l_dragon = pygame.image.load("sprites/l_dragonb.png")
-r_dragon = pygame.image.load("sprites/r_dragonb.png")
-bow_sound = pygame.mixer.Sound("sound/bow_sound.wav")
-bow_hit_sound = pygame.mixer.Sound("sound/bow_hit_sound.wav")
-punch_sound = pygame.mixer.Sound("sound/punch.wav")
+walkRight = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/zelda_Rwalk2b.png",6,1)
+walkLeft = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/zelda_Lwalk2b.png",6,1)     #100 X 120 px
+standing = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/zelda_standing2.png",1,1)
+background = pygame.image.load("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/wallpaper.gif")
+l_punch = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/zelda_Lpunchb.png",1,1)
+r_punch = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/zelda_Rpunchb.png",1,1)
+l_bow = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/zelda_Lbowb.png",4,1)
+r_bow = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/zelda_Rbowb.png",4,1)       #corrigir as imagens
+l_arrow = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/l_arrow.png",1,1)
+r_arrow = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/r_arrow.png",1,1)
+r_ghost = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/r_ghostb.gif",1,1)
+l_ghost = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/l_ghostb.png",1,1)
+r_koopa = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/koopas_rightb.gif",3,1)
+l_koopa = spritesheet("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/koopas_leftb.gif",3,1)
+l_dragon = pygame.image.load("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/l_dragonb.png")
+r_dragon = pygame.image.load("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sprites/r_dragonb.png")
+bow_sound = pygame.mixer.Sound("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sound/bow_sound.wav")
+bow_hit_sound = pygame.mixer.Sound("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sound/bow_hit_sound.wav")
+punch_sound = pygame.mixer.Sound("C:/Users/joaog/OneDrive/Desktop/FEUP/1 ano/fpro/Kongfu/sound/punch.wav")
 
 
 
@@ -252,30 +252,38 @@ class projectiles(object):
 
 def redrawGameWindow():
     global walkCount
-    win.blit(background,(0,170))
-    pygame.draw.rect(win,(80,30,0), (0,0,W,170))
-    pygame.draw.rect(win,(80,30,0), (0,720-147,W,170))
-    win.blit(l_dragon,(-10,0))
-    win.blit(r_dragon,(W-110,0))
-    title = font3.render("Kong Fu", 1, (255,255,255))
-    score_count = font2.render("Score: " + str(score), 1, (255,255,255))
-    win.blit(title,(W/2-100,50))
-    win.blit(score_count, (W-200,120))
-    man.draw(win)
-    for arrow in arrows:
-        arrow.draw(win)
-    for enemy in enemies:
-        enemy.draw(win)
+    global run
+    if run:
+        win.blit(background,(0,170))
+        pygame.draw.rect(win,(80,30,0), (0,0,W,170))
+        pygame.draw.rect(win,(80,30,0), (0,720-147,W,170))
+        win.blit(l_dragon,(-10,0))
+        win.blit(r_dragon,(W-110,0))
+        title = font3.render("Kong Fu", 1, (255,255,255))
+        score_count = font2.render("Score: " + str(score), 1, (255,255,255))
+        win.blit(title,(W/2-100,50))
+        win.blit(score_count, (W-200,120))
+        man.draw(win)
+        for arrow in arrows:
+            arrow.draw(win)
+        for enemy in enemies:
+            enemy.draw(win)
     #ghost.draw(win,0,300,400,4)
+    else:
+        pygame.time.delay(100)
+        win.fill(BLACK)
+        text = font4.render("Game over", 1, (255,0,0))
+        win.blit(text, (W/2-340,H/2-150))
+        pygame.display.update()
+        pygame.time.delay(1000)
     pygame.display.update()
-
-    
 
 #main loop
 man = player(300,500,110,150)   #Check width and height
 font1 = pygame.font.SysFont('comicsans', 70, False, True) #(font, size, bold, italicized)
 font2 = pygame.font.SysFont('comicsans', 40, False, False) #(font, size, bold, italicized)
 font3 = pygame.font.SysFont('javanesetext', 55, False, False)
+font4 = pygame.font.SysFont('vivaldi',180, False, False)
 arrows = []
 #koopa = enemy(100,500,90,90,450)
 enemies = []
@@ -285,6 +293,7 @@ hit_count = 0
 
 
 while run:
+
     CLOCK.tick(FPS)
 
     for event in pygame.event.get():
@@ -374,6 +383,9 @@ while run:
         #man.punchCount = 0
         #man.kickCount = 0
 
-
+    if man.health <= 0:
+        run = False
 
     redrawGameWindow()
+    
+
